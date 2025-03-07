fd8360df6c2205c3da7fcb476513aea2614d761b
Alex Kozin <al@el-machine.com>
2025-03-07 21:36:53 +0900

    Update to using Wand organization

--- Package.swift
+++ Package.swift
@@ -35,8 +35,8 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/el-machine/Any.git", from: "1.0.1"),
-        .package(url: "https://github.com/el-machine/Wand.git", from: "1.5.2"),
+        .package(url: "https://github.com/The-Wand/Any.git", from: "1.0.1"),
+        .package(url: "https://github.com/The-Wand/Wand.git", from: "1.5.2"),
     ],
 
     targets: [
