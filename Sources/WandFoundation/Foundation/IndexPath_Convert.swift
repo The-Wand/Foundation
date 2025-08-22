e716d0ff9214fdaf5f702219ef9a230eb21a7c1d
Alex Kozin <al@el-machine.com>
2025-08-22 15:20:22 +0700

    Add IndePath feature

---
+++ Sources/WandFoundation/Foundation/IndexPath_Convert.swift
@@ -0,0 +1,45 @@
+///
+/// Copyright 2020 Alexander Kozin
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Created by Alex Kozin
+/// El Machine 🤖
+
+#if canImport(Foundation)
+import Foundation.NSIndexPath
+import Wand
+
+/// Convert
+///
+/// let path: IndexPath = (0, 1)|
+///
+@inline(__always)
+postfix
+public
+func | (raw: (Int, Int)) -> IndexPath {
+    .init(row: raw.0, section: raw.1)
+}
+
+/// Convert
+///
+/// let tuple = path|
+///
+@inline(__always)
+postfix
+public
+func | (path: IndexPath) -> (Int, Int) {
+    (path.row, path.section)
+}
+
+#endif
